e422c9641353405bd0ec21751c79babbed0cec92
Пользователь <trollka@list.ru>
2023-06-04 20:42:16 +0300

    add posts

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template, url_for
-from flask.sqlalchemy import SQLAlchemy
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 class Article(db.Model):
@@ -13,7 +15,7 @@ class Article(db.Model):
     date = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
-        return '<Artilce %r' %self.id
+        return '<Artilce %r>' %self.id
 
 
 @app.route('/')
@@ -25,10 +27,35 @@ def index():
 def about():
     return render_template('about.html')
 
-@app.route('/user/<string:name>/<int:id>')
-def user(name,id):
-    return 'User page: ' + name + '-' + str(id)
+@app.route('/posts')
+def posts():
+    articles = Article.query.order_by(Article.date).all()
+    return render_template('posts.html', articles=articles)
 
-if __name__== '__main__':
+# @app.route('/user/<string:name>/<int:id>')
+# def user(name,id):
+#     return 'User page: ' + name + '-' + str(id)
+
+@app.route('/create-article', methods=['POST', 'GET'])
+def create_article():
+    if request.method == 'POST':
+        title = request.form['title']
+        intro = request.form['intro']
+        text = request.form['text']
+
+        article = Article(title=title, intro=intro, text=text)
+
+        try:
+            db.session.add(article)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'При добавлении статьи произошла ошибка'
+    else:
+        return render_template('create-article.html')
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
 
